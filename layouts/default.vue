2009120343d2b335f88f67d688b038e87d3aeb53
Pietro Stivanello <68153217+pedrito-96@users.noreply.github.com>
2024-03-02 01:36:22 +0100

    feat: index page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-[calc(100vh_-_96px)] p-4 z-0">
+  <div>
     <slot />
   </div>
 </template>
---
+++ layouts/landingLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>
+      <img src="/assets/icons/reaskLogo.svg" class="absolute top-40 left-40" />
+    </div>
+    <slot />
+  </div>
+</template>
