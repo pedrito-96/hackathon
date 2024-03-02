5c5d8461d0f9469ec0080c7c1ed33e3f0845f3f2
LuciaFe <100196416+LuciaFe@users.noreply.github.com>
2024-03-02 03:02:45 +0100

    feat(lucia): accordion

---
+++ components/Organisms/OrganismsAccordionList.props.ts
@@ -0,0 +1,5 @@
+import type { AccordionProps } from "../Molecules/MoleculesAccordion.props";
+
+export type AccordionListProps = {
+  list: AccordionProps;
+};
---
+++ components/Organisms/OrganismsAccordionList.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { AccordionListProps } from "./OrganismsAccordionList.props";
+defineProps<AccordionListProps>();
+</script>
+
+<template>
+  <div class="flex flex-col divide-y-[1px] divide-gray-400">
+    <div v-for="acc in list">
+      <MoleculesAccordion v-bind="acc"></MoleculesAccordion>
+    </div>
+  </div>
+</template>
