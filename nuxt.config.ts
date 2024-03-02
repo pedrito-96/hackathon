eed4df2756e2dc444007b9bcf8803f1559e63f82
LuciaFe <100196416+LuciaFe@users.noreply.github.com>
2024-03-02 01:03:39 +0100

    feat(lucia): search

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,47 @@
+import Components from "unplugin-vue-components/vite";
+import { HeadlessUiResolver } from "unplugin-vue-components/resolvers";
+
+import IconsResolver from "unplugin-icons/resolver";
+import Icons from "unplugin-icons/vite";
+import { FileSystemIconLoader } from "unplugin-icons/loaders";
+import { fileURLToPath } from "url";
+
 export default defineNuxtConfig({
   app: {
     head: {
       title: "Hackathon 2024",
     },
   },
-
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [
+          IconsResolver({
+            prefix: false,
+            customCollections: ["icons"],
+          }),
+          HeadlessUiResolver(),
+        ],
+        dts: true,
+      }),
+      Icons({
+        customCollections: {
+          icons: FileSystemIconLoader(
+            fileURLToPath(new URL("./icons/", import.meta.url)),
+            (svg) => {
+              const viewBox = /viewBox="\d+ \d+ (\d+) (\d+)"/gi.exec(svg);
+              if (!viewBox) return svg;
+              const w = viewBox?.[1];
+              const h = viewBox?.[2];
+              svg = svg.replace(/width=".*?"/, `width="${w}" `);
+              svg = svg.replace(/height=".*?"/, `height="${h}" `);
+              return svg;
+            }
+          ),
+        },
+      }),
+    ],
+  },
   modules: [
     "@formkit/nuxt",
     "@nuxtjs/tailwindcss",
---
+++ pages/test/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const v = ref("");
+</script>
+
+<template>
+  <MoleculesTextInput
+    label="label"
+    placeholder="placeholder"
+    @onEnter="
+      (value) => {
+        console.log(value);
+        v = value;
+      }
+    "
+  />
+  <div v-if="v" class="bg-black text-white">{{ v }}</div>
+</template>
